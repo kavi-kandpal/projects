7b63fe5f4cf0ef8a165d2b0b9e72e12fe25653d1
kavi <kavikandpal123456@gmail.com>
2024-12-13 13:27:42 +0530

    second commit

--- spotify_project/phase.js
+++ spotify_project/phase.js
@@ -1,40 +1,40 @@
 console.log("welcome to spotify");
-let songindex=0;
-let audio=new Audio('music/1.mp3');
-let masterplay=document.getElementById('masterplay');
-let myprogress=document.getElementById('progress');
-let gif=document.getElementById('gif');
-let songs=[
-    
-    {songname:"1.mp3" ,filepath: "music/1.mp3"},
-    {songname:"2.mp3" ,filepath: "music/2.mp3"},
-    {songname:"3.mp3" ,filepath: "music/3.mp3"},
-    {songname:"4.mp3" ,filepath: "music/4.mp3"},
-    {songname:"5.mp3" ,filepath: "music/5.mp3"},
-    {songname:"6.mp3" ,filepath: "music/6.mp3"},
-    {songname:"7.mp3" ,filepath: "music/7.mp3"},
-    {songname:"8.mp3" ,filepath: "music/8.mp3"},
-    {songname:"9.mp3" ,filepath: "music/9.mp3"},
+let songindex = 0;
+let audio = new Audio('music/1.mp3');
+let masterplay = document.getElementById('masterplay');
+let myprogress = document.getElementById('progress');
+let gif = document.getElementById('gif');
+let songs = [
+
+    { songname: "1.mp3", filepath: "music/1.mp3" },
+    { songname: "2.mp3", filepath: "music/2.mp3" },
+    { songname: "3.mp3", filepath: "music/3.mp3" },
+    { songname: "4.mp3", filepath: "music/4.mp3" },
+    { songname: "5.mp3", filepath: "music/5.mp3" },
+    { songname: "6.mp3", filepath: "music/6.mp3" },
+    { songname: "7.mp3", filepath: "music/7.mp3" },
+    { songname: "8.mp3", filepath: "music/8.mp3" },
+    { songname: "9.mp3", filepath: "music/9.mp3" },
 
 ]
-  
-  masterplay.addEventListener('click',()=>{
+//   
+masterplay.addEventListener('click', () => {
     masterplay.addEventListener('click', () => {
         if (audio.paused || audio.currentTime <= 0) {
             audio.play();
             masterplay.classList.remove('fa-play-circle');
             masterplay.classList.add('fa-pause-circle');
-            gif.style.opacity=1;
-        } 
+            gif.style.opacity = 1;
+        }
         else {
             audio.pause();
             masterplay.classList.remove('fa-pause-circle');
             masterplay.classList.add('fa-play-circle');
-            gif.style.opacity=0;
+            gif.style.opacity = 0;
 
         }
     });
-  })
+})
 audio.addEventListener('timeupdate', () => {
     let currentProgress = (audio.currentTime / audio.duration) * 100;
     myprogress.value = currentProgress;
@@ -53,7 +53,7 @@ const play = () => {
 };
 Array.from(document.getElementsByClassName('songplay')).forEach((element) => {
     element.addEventListener('click', (e) => {
-        play(); 
+        play();
         // Get the index of the clicked song based on its id
         songindex = parseInt(e.target.id);
 
@@ -72,12 +72,12 @@ Array.from(document.getElementsByClassName('songplay')).forEach((element) => {
         masterplay.classList.add('fa-pause-circle');
     });
 });
-document.getElementById("next").addEventListener("click",()=>{
-    if(songindex>=9){
-        songindex=0;
+document.getElementById("next").addEventListener("click", () => {
+    if (songindex >= 9) {
+        songindex = 0;
     }
-    else{
-        songindex+=1;
+    else {
+        songindex += 1;
     }
     audio.src = `music/${songindex}.mp3`;
 
@@ -89,12 +89,12 @@ document.getElementById("next").addEventListener("click",()=>{
     masterplay.classList.remove('fa-play-circle');
     masterplay.classList.add('fa-pause-circle');
 });
-document.getElementById("back",()=>{
-    if(songindex==1){
-        songindex=9;
+document.getElementById("back", () => {
+    if (songindex == 1) {
+        songindex = 9;
     }
-    else{
-        songindex-=1;
+    else {
+        songindex -= 1;
     }
     audio.src = `music/${songindex}.mp3`;
 
